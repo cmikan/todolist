ed9c8743e09132542bfb7bc7cc7b8e1fb9094bce
mionosaur <llmilico@proton.me>
2024-05-27 14:05:23 +0200

    [+] db initialization

--- src/todolist.c
+++ src/todolist.c
@@ -1,9 +1,42 @@
 #include <sqlite3.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
     sqlite3 *db;
+    char *err_msg = 0;
+    int rv;
+    const char *db_filename = "todolist.db";
 
-    sqlite3_open("todolist.db", &db);
+// Check if db already exist
+    int init_db = 0;
+    if (access(db_filename, F_OK) == -1)
+    {
+        init_db = 1;
+    }
+
+    rv = sqlite3_open("todolist.db", &db);
+
+    if (rv != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return -1;
+    }
+
+    if (init_db)
+    {
+        rv = sqlite3_exec(db, "CREATE TABLE \"\" (\"id\" INTEGER NOT NULL UNIQUE, \"note\" TEXT NOT NULL,PRIMARY KEY(\"id\" AUTOINCREMENT));", 0, 0, &err_msg);
+        if (rv != SQLITE_OK)
+        {
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+            return 1;
+        }
+    }
+
+    sqlite3_close(db);
     return 0;
 }
