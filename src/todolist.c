1c83ca53e4a5895ae5a4f6ecb7ea4225098ecafe
mionosaur <llmilico@proton.me>
2024-05-27 14:53:46 +0200

    [+] view and insert modes (ugly edition)

--- src/todolist.c
+++ src/todolist.c
@@ -1,6 +1,9 @@
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+int callback(void *NotUsed, int argc, char **argv, char **azColName);
 
 int main(int argc, char **argv)
 {
@@ -33,16 +36,55 @@ int main(int argc, char **argv)
 
     if (init_db)
     {
-        rv = sqlite3_exec(db, "CREATE TABLE \"\" (\"id\" INTEGER NOT NULL UNIQUE, \"note\" TEXT NOT NULL,PRIMARY KEY(\"id\" AUTOINCREMENT));", 0, 0, &err_msg);
+        rv = sqlite3_exec(db, "CREATE TABLE \"notes\" (\"id\" INTEGER NOT NULL UNIQUE, \"note\" TEXT NOT NULL,PRIMARY KEY(\"id\" AUTOINCREMENT));", 0, 0, &err_msg);
         if (rv != SQLITE_OK)
         {
             fprintf(stderr, "SQL error: %s\n", err_msg);
             sqlite3_free(err_msg);
             sqlite3_close(db);
-            return 1;
+            return -1;
+        }
+    }
+
+    if (argc == 1) // View mode
+    {
+        printf("Notes in the list:\n");
+        char *insert_stm = sqlite3_mprintf("SELECT id, note FROM notes");
+        rv = sqlite3_exec(db, insert_stm, callback, 0, &err_msg);
+        if (rv != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+            return -1;
+        }
+    }
+    else if (argc == 2)
+    {
+        if (atoi(argv[1])) // Delete mode
+        {
+            return 0;
+        }
+        else // Insert mod
+        {
+            char *insert_stm = sqlite3_mprintf("INSERT INTO notes (note) VALUES ('%q');", argv[1]);
+            char *err_msg = 0;
+            int rc = sqlite3_exec(db, insert_stm, 0, 0, &err_msg);
+            if (rc != SQLITE_OK) {
+                fprintf(stderr, "SQL error: %s\n", err_msg);
+                sqlite3_free(err_msg);
+            } else {
+                printf("Note added successfully.\n");
+            }
+
+            sqlite3_free(insert_stm);
         }
     }
 
     sqlite3_close(db);
+    return 0;
+}
+
+int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    printf("%s. %s\n", argv[0], argv[1]);
     return 0;
 }
